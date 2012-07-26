3f20d378bd6cc481f9575b0803233c018b9b43f7
Sean Treadway <sean@soundcloud.com>
2012-07-26 13:33:32 +0200

    Reliable publishing via confirm.select

--- examples/simple-producer/producer.go
+++ examples/simple-producer/producer.go
@@ -12,9 +12,11 @@ import (
 
 var (
 	uri          = flag.String("uri", "amqp://guest:guest@localhost:5672/", "AMQP URI")
-	exchangeName = flag.String("exchange", "test-exchange", "AMQP exchange name")
-	routingKey   = flag.String("routing-key", "test-key", "AMQP routing key")
+	exchangeName = flag.String("exchange", "test-exchange", "Durable AMQP exchange name")
+	exchangeType = flag.String("exchange-type", "direct", "Exchange type - direct|fanout|topic|x-custom")
+	routingKey   = flag.String("key", "test-key", "AMQP routing key")
 	body         = flag.String("body", "foobar", "Body of message")
+	reliable     = flag.Bool("reliable", true, "Wait for the publisher confirmation before exiting")
 )
 
 func init() {
@@ -22,13 +24,13 @@ func init() {
 }
 
 func main() {
-	if err := publish(*uri, *exchangeName, *routingKey, *body); err != nil {
+	if err := publish(*uri, *exchangeName, *exchangeType, *routingKey, *body, *reliable); err != nil {
 		log.Fatalf("%s", err)
 	}
 	log.Printf("published %dB OK", len(*body))
 }
 
-func publish(amqpURI, exchange, routingKey, body string) error {
+func publish(amqpURI, exchange, exchangeType, routingKey, body string, reliable bool) error {
 
 	// This function dials, connects, declares, publishes, and tears down,
 	// all in one go. In a real service, you probably want to maintain a
@@ -39,25 +41,36 @@ func publish(amqpURI, exchange, routingKey, body string) error {
 	if err != nil {
 		return fmt.Errorf("Dial: %s", err)
 	}
+	defer connection.Close()
 
 	log.Printf("got Connection, getting Channel")
 	channel, err := connection.Channel()
 	if err != nil {
 		return fmt.Errorf("Channel: %s", err)
 	}
-	defer closeChannel(channel)
 
-	log.Printf("got Channel, declaring Exchange (%s)", exchange)
-	noArgs := amqp.Table{}
+	log.Printf("got Channel, declaring %q Exchange (%s)", exchangeType, exchange)
 	if err := channel.ExchangeDeclare(
 		exchange,          // name
 		amqp.UntilDeleted, // lifetime = durable
-		"direct",          // type
+		exchangeType,      // type
 		false,             // internal
 		false,             // noWait
-		noArgs,            // arguments
+		nil,               // arguments
 	); err != nil {
 		return fmt.Errorf("Exchange Declare: %s", err)
+	}
+
+	// Reliable publisher confirms require confirm.select support from the
+	// connection.
+	if reliable {
+		if err := channel.Confirm(false); err != nil {
+			return fmt.Errorf("Channel could not be put into confirm mode: %s", err)
+		}
+
+		ack, nack := channel.NotifyConfirm(make(chan uint64), make(chan uint64))
+
+		defer confirmOne(ack, nack)
 	}
 
 	log.Printf("declared Exchange, publishing %dB body (%s)", len(body), body)
@@ -71,8 +84,8 @@ func publish(amqpURI, exchange, routingKey, body string) error {
 			ContentType:     "text/plain",
 			ContentEncoding: "",
 			Body:            []byte(body),
-			DeliveryMode:    1, // 1=non-persistent, 2=persistent
-			Priority:        0, // 0-9
+			DeliveryMode:    amqp.TransientDelivery, // 1=non-persistent, 2=persistent
+			Priority:        0,                      // 0-9
 			// a bunch of application/implementation-specific fields
 		},
 	); err != nil {
@@ -82,8 +95,16 @@ func publish(amqpURI, exchange, routingKey, body string) error {
 	return nil
 }
 
-func closeChannel(c *amqp.Channel) {
-	if err := c.Close(); err != nil {
-		log.Printf("AMQP channel close error: %s", err)
+// One would typically keep a channel of publishings, a sequence number, and a
+// set of unacknowledged sequence numbers and loop until the publishing channel
+// is closed.
+func confirmOne(ack, nack chan uint64) {
+	log.Printf("waiting for confirmation of one publishing")
+
+	select {
+	case tag := <-ack:
+		log.Printf("confirmed delivery with delivery tag: %d", tag)
+	case tag := <-nack:
+		log.Printf("failed delivery of delivery tag: %d", tag)
 	}
 }
