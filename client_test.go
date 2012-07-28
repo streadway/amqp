30989b9c6af50e77bf96982b0476aac971c88143
Sean Treadway <sean@soundcloud.com>
2012-07-28 14:35:13 +0200

    Refactor protocol drivers into session DSL
    Grants independence from integration tests to test expected return
    values, deadlocks or other logic that client library has that isn't
    directly related to what's on the wire.

--- client_test.go
+++ client_test.go
@@ -8,119 +8,180 @@ package amqp
 import (
 	"bytes"
 	"io"
+	"reflect"
 	"testing"
+	"time"
 )
 
-func driveConnectionOpen(t *testing.T, server io.ReadWriter) {
-	var f frame
-	var err error
-	var ok bool
+type server struct {
+	*testing.T
+	r reader        // framer <- client
+	w writer        // framer -> client
+	S io.ReadWriter // Server IO
+	C io.ReadWriter // Client IO
+}
 
-	handshake := make([]byte, 8)
-	server.Read(handshake)
-	if bytes.Compare(handshake, []byte{'A', 'M', 'Q', 'P', 0, 0, 9, 1}) != 0 {
-		t.Fatalf("bad protocol handshake: %s", handshake)
+func defaultConfig() Config {
+	return Config{SASL: []Authentication{&PlainAuth{"guest", "guest"}}, Vhost: "/"}
+}
+
+func newSession(t *testing.T) (io.ReadWriteCloser, *server) {
+	type pipe struct {
+		io.Reader
+		io.WriteCloser
 	}
 
-	r := reader{r: server}
-	w := writer{w: server}
+	rs, wc := io.Pipe()
+	rc, ws := io.Pipe()
 
-	if err = w.WriteFrame(&methodFrame{
-		ChannelId: 0,
-		Method: &connectionStart{
-			VersionMajor: 0,
-			VersionMinor: 9,
-			Mechanisms:   "PLAIN",
-			Locales:      "en-us",
-		},
-	}); err != nil {
-		t.Fatalf("bad write")
+	rws := &logIO{t, "server", pipe{rs, ws}}
+	rwc := &logIO{t, "client", pipe{rc, wc}}
+
+	server := server{
+		T: t,
+		r: reader{rws},
+		w: writer{rws},
+		S: rws,
+		C: rwc,
 	}
 
-	if f, err = r.ReadFrame(); err != nil {
-		t.Fatalf("bad read: %s", err)
-	}
-
-	if _, ok = f.(*methodFrame).Method.(*connectionStartOk); !ok {
-		t.Fatalf("expected ConnectionStartOk")
-	}
-
-	if err = w.WriteFrame(&methodFrame{
-		ChannelId: 0,
-		Method: &connectionTune{
-			ChannelMax: 11,
-			FrameMax:   20000,
-			Heartbeat:  10,
-		},
-	}); err != nil {
-		t.Fatalf("bad write: %s", err)
-	}
-
-	if f, err = r.ReadFrame(); err != nil {
-		t.Fatalf("bad read: %s", err)
-	}
+	return rwc, &server
+}
 
 	if _, ok = f.(*methodFrame).Method.(*connectionTuneOk); !ok {
 		t.Fatalf("expected ConnectionTuneOk")
 	}
 
-	if f, err = r.ReadFrame(); err != nil {
-		t.Fatalf("bad read: %s", err)
+func (t *server) expectBytes(b []byte) {
+	in := make([]byte, len(b))
+	if _, err := io.ReadFull(t.S, in); err != nil {
+		t.Fatalf("io error expecting bytes: %v", err)
 	}
 
-	if _, ok = f.(*methodFrame).Method.(*connectionOpen); !ok {
-		t.Fatalf("expected ConnectionOpen")
-	}
-
-	if err = w.WriteFrame(&methodFrame{
-		ChannelId: 0,
-		Method:    &connectionOpenOk{},
-	}); err != nil {
-		t.Fatalf("bad write: %s", err)
+	if bytes.Compare(b, in) != 0 {
+		t.Fatalf("failed bytes: expected: %s got: %s", string(b), string(in))
 	}
 }
 
-func driveChannelOpen(t *testing.T, server io.ReadWriteCloser) {
-	var f frame
-	var err error
-	var ok bool
+func (t *server) send(channel int, m message) {
+	defer time.AfterFunc(10*time.Millisecond, func() { panic("send deadlock") }).Stop()
 
-	r := reader{r: server}
-	w := writer{w: server}
-
-	if f, err = r.ReadFrame(); err != nil {
-		t.Fatalf("bad read: %s", err)
-	}
-
-	if _, ok = f.(*methodFrame).Method.(*channelOpen); !ok {
-		t.Fatalf("expected channelOpen")
-	}
-
-	if err = w.WriteFrame(&methodFrame{
-		ChannelId: f.channel(),
-		Method:    &channelOpenOk{},
+	if err := t.w.WriteFrame(&methodFrame{
+		ChannelId: uint16(channel),
+		Method:    m,
 	}); err != nil {
-		t.Fatalf("bad write")
+		t.Fatalf("frame err, write: %s", err)
 	}
+}
+
+// Currently drops all but method frames expected on the given channel
+func (t *server) recv(channel int, m message) message {
+	defer time.AfterFunc(10*time.Millisecond, func() { panic("recv deadlock") }).Stop()
+
+	var remaining int
+	var header *headerFrame
+	var body []byte
+
+	for {
+		frame, err := t.r.ReadFrame()
+		if err != nil {
+			t.Fatalf("frame err, read: %s", err)
+		}
+
+		if frame.channel() != uint16(channel) {
+			t.Fatalf("expected frame on channel %d, got channel %d", channel, frame.channel())
+		}
+
+		switch f := frame.(type) {
+		case *heartbeatFrame:
+			// drop
+
+		case *headerFrame:
+			// start content state
+			header = f
+			remaining = int(header.Size)
+
+		case *bodyFrame:
+			// continue until terminated
+			body = append(body, f.Body...)
+			remaining -= len(f.Body)
+			if remaining <= 0 {
+				m.(messageWithContent).setContent(header.Properties, body)
+				return m
+			}
+
+		case *methodFrame:
+			if reflect.TypeOf(m) == reflect.TypeOf(f.Method) {
+				wantv := reflect.ValueOf(m).Elem()
+				havev := reflect.ValueOf(f.Method).Elem()
+				wantv.Set(havev)
+				if _, ok := m.(messageWithContent); !ok {
+					return m
+				}
+			} else {
+				t.Fatalf("expected method type: %T, got: %T", m, f.Method)
+			}
+		}
+	}
+
+	panic("unreachable")
+}
+
+func (t *server) expectAMQP() {
+	t.expectBytes([]byte{'A', 'M', 'Q', 'P', 0, 0, 9, 1})
+}
+
+func (t *server) connectionOpen() {
+	t.expectAMQP()
+
+	t.send(0, &connectionStart{
+		VersionMajor: 0,
+		VersionMinor: 9,
+		Mechanisms:   "PLAIN",
+		Locales:      "en-us",
+	})
+
+	t.recv(0, &connectionStartOk{})
+
+	t.send(0, &connectionTune{
+		ChannelMax: 11,
+		FrameMax:   20000,
+		Heartbeat:  10,
+	})
+
+	t.recv(0, &connectionTuneOk{})
+	t.recv(0, &connectionOpen{})
+	t.send(0, &connectionOpenOk{})
+}
+
+func (t *server) connectionClose() {
+	t.recv(0, &connectionClose{})
+	t.send(0, &connectionCloseOk{})
+}
+
+func (t *server) channelOpen(id int) {
+	t.recv(id, &channelOpen{})
+	t.send(id, &channelOpenOk{})
 }
 
 func TestNewConnectionOpen(t *testing.T) {
-	server, client := interPipes(t)
+	rwc, srv := newSession(t)
+	go srv.connectionOpen()
 
-	go driveConnectionOpen(t, server)
-
-	c, err := NewConnection(client, Config{SASL: []Authentication{&PlainAuth{"guest", "guest"}}, Vhost: "/"})
-	if err != nil {
+	if c, err := NewConnection(rwc, defaultConfig()); err != nil {
 		t.Fatalf("could not create connection: %s (%s)", c, err)
 	}
 }
 
 func TestNewConnectionChannelOpen(t *testing.T) {
-	server, client := interPipes(t)
+	rwc, srv := newSession(t)
 
-	go driveConnectionOpen(t, server)
+	go func() {
+		srv.connectionOpen()
+		srv.channelOpen(1)
+	}()
 
-	c, err := NewConnection(client, Config{SASL: []Authentication{&PlainAuth{"guest", "guest"}}, Vhost: "/"})
+	c, err := NewConnection(rwc, defaultConfig())
 	if err != nil {
 		t.Fatalf("could not create connection: %s (%s)", c, err)
 	}
--- lib_test.go
+++ lib_test.go
@@ -9,6 +9,7 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/binary"
+	"encoding/hex"
 	"hash/crc32"
 	"io"
 	"os"
@@ -16,6 +17,49 @@ import (
 	"time"
 )
 
+type logIO struct {
+	t      *testing.T
+	prefix string
+	proxy  io.ReadWriteCloser
+}
+
+func (me *logIO) Read(p []byte) (n int, err error) {
+	me.t.Logf("%s reading %d\n", me.prefix, len(p))
+	n, err = me.proxy.Read(p)
+	if err != nil {
+		me.t.Logf("%s read %x: %v\n", me.prefix, p[0:n], err)
+	} else {
+		me.t.Logf("%s read:\n%s\n", me.prefix, hex.Dump(p[0:n]))
+		//fmt.Printf("%s read:\n%s\n", me.prefix, hex.Dump(p[0:n]))
+	}
+	return
+}
+
+func (me *logIO) Write(p []byte) (n int, err error) {
+	me.t.Logf("%s writing %d\n", me.prefix, len(p))
+	n, err = me.proxy.Write(p)
+	if err != nil {
+		me.t.Logf("%s write %d, %x: %v\n", me.prefix, len(p), p[0:n], err)
+	} else {
+		me.t.Logf("%s write %d:\n%s", me.prefix, len(p), hex.Dump(p[0:n]))
+		//fmt.Printf("%s write %d:\n%s", me.prefix, len(p), hex.Dump(p[0:n]))
+	}
+	return
+}
+
+func (me *logIO) Close() (err error) {
+	err = me.proxy.Close()
+	if err != nil {
+		me.t.Logf("%s close : %v\n", me.prefix, err)
+	} else {
+		me.t.Logf("%s close\n", me.prefix, err)
+	}
+	return
+}
+
+func (me *logIO) Test() {
+	me.t.Logf("test: %v\n", me)
+}
 func integrationUri(t *testing.T) (*URI, bool) {
 	urlStr := os.Getenv("AMQP_URL")
 	if urlStr == "" {
