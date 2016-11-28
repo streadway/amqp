c6f897a0253ab12ddd64fdafcee2e78e365c9942
DmitriyMV <codeprojtwo@gmail.com>
2016-11-28 17:21:28 +0300

    Check err != nil before using our connection in tests. Prevents panic on nil pointer access.

--- connection_test.go
+++ connection_test.go
@@ -7,12 +7,15 @@ import (
 
 func TestChannelOpenOnAClosedConnectionFails(t *testing.T) {
 	conn, err := Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Could't connect to amqp server, err = %s", err)
+	}
 	conn.Close()
 
 	if !conn.IsClosed() {
 		log.Fatalf("connection %s is expected to be closed", conn)
 	}
-	
+
 	_, err = conn.Channel()
 	if err != ErrClosed {
 		log.Fatalf("channel.open on a closed connection %s is expected to fail", conn)
@@ -21,14 +24,17 @@ func TestChannelOpenOnAClosedConnectionFails(t *testing.T) {
 
 func TestQueueDeclareOnAClosedConnectionFails(t *testing.T) {
 	conn, err := Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Could't connect to amqp server, err = %s", err)
+	}
 	ch, _ := conn.Channel()
-	
+
 	conn.Close()
 
 	if !conn.IsClosed() {
 		log.Fatalf("connection %s is expected to be closed", conn)
 	}
-	
+
 	_, err = ch.QueueDeclare("an example", false, false, false, false, nil)
 	if err != ErrClosed {
 		log.Fatalf("queue.declare on a closed connection %s is expected to fail", conn)
