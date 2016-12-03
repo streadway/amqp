1571aa2b663b4f11f5b0e03cb76470a8b1adc069
Michael Klishin <michael@clojurewerkz.org>
2016-12-04 01:56:03 +0300

    Accept net.OpErrors in this test, too
    If a connection.close operation fails before the connection is marked
    as closed internally, it's still a reasonable error for Connection#Close to return.
    
    Note that it only happens with a certain degree of concurrency
    in this test and thus the occurrence is quite unlikely in practice.
    
    References #227.

--- connection_test.go
+++ connection_test.go
@@ -2,6 +2,7 @@ package amqp
 
 import (
 	"log"
+	"net"
 	"sync"
 	"testing"
 )
@@ -48,13 +49,22 @@ func TestConcurrentClose(t *testing.T) {
 		log.Fatalf("Could't connect to amqp server, err = %s", err)
 	}
 
+	n := 32
 	wg := sync.WaitGroup{}
-	wg.Add(5)
-	for i := 0; i < 5; i++ {
+	wg.Add(n)
+	for i := 0; i < n; i++ {
 		go func() {
 			err := conn.Close()
 			if err != nil && err != ErrClosed {
-				log.Fatalf("Expected nil or ErrClosed - got %#v, type is %T", err, err)
+				switch err.(type) {
+				case *Error:
+					log.Fatalf("Expected no error, or ErrClosed, or a net.OpError from conn.Close(), got %#v (%#v) of type %T", err, err.Error(), err)
+				case *net.OpError:
+					// this is acceptable: we got a net.OpError
+					// before the connection was marked as closed
+					break;
+				}
+
 			}
 			wg.Done()
 		}()
