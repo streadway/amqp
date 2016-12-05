a7c6cd63fe6d7371160c9fec4082c68a3cb4b09e
Michael Klishin <michael@clojurewerkz.org>
2016-12-05 08:18:35 +0300

    Revert "Merge branch 'cbandy-tls-connection-state'"
    This reverts commit 546e912e8e3d9921082cb1d853d1434efcc9d2fb, reversing
    changes made to d549c4c354d7518d82666770ff5c91b5bf469a61.
    
    Per request from @streadway. There are reasons to use a different
    function name.

--- tls_test.go
+++ tls_test.go
@@ -83,7 +83,7 @@ func TestTLSHandshake(t *testing.T) {
 	cert, _ := tls.X509KeyPair([]byte(clientCert), []byte(clientKey))
 	cfg.Certificates = append(cfg.Certificates, cert)
 
-	c, err := amqp.DialTLS(srv.URL, cfg)
+	_, err := amqp.DialTLS(srv.URL, cfg)
 
 	select {
 	case <-time.After(10 * time.Millisecond):
@@ -92,11 +92,6 @@ func TestTLSHandshake(t *testing.T) {
 		if string(header) != "AMQP" {
 			t.Fatalf("expected to handshake a TLS connection, got err: %v", err)
 		}
-	}
-
-	st := c.TLSConnectionState()
-	if !st.HandshakeComplete {
-		t.Errorf("TLS handshake failed, TLS connection state: %+v", st)
 	}
 }
 
